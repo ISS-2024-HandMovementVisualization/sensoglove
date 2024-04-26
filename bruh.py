d214f2ec882247a25986c2129fcdef31bb8ac865
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-04-26 19:24:56 +0200

    chyba works

--- bruh.py
+++ bruh.py
@@ -1,6 +1,7 @@
 import serial
 import requests
 from timeit import default_timer as timer
+import random
 
 ser = serial.Serial(
     port='COM6',\
@@ -16,21 +17,22 @@ line = ""
 sensor_line = None
 last_send = timer()
 
-# requests.post("http://192.168.0.145:8080", data="aaaa")
-# exit()
+def send(data):
+    requests.post("http://192.168.0.145:8080", data=data)
 
-class Sender:
+class SerialParser:
     def __init__(self):
         self.line = ""
         self.sensor_line = ""
         self.last_send = timer()
-        self.interval_s = 5
-        self.dry = True
+        self.interval_s = 0.1
+        self.dry = False
 
     def advance(self, char):
         self.line += char
         if char == "\n":
-            if self.line.strip().startswith("R"):
+            print(self.line)
+            if self.line.strip().startswith(">"):
                 self.got_sensor_line(self.line)
             self.line = ""
 
@@ -38,20 +40,34 @@ class Sender:
         self.sensor_line = self.process_sensor_line(line)
         print(self.sensor_line)
         elapsed = timer() - self.last_send
-        if elapsed >= self.interval_s and sensor_line:
+        print(elapsed)
+        if elapsed >= self.interval_s and self.sensor_line:
             if not self.dry:
-                requests.post("http://192.168.0.145:8080", data=sensor_line)
+                print("sending")
+                send(self.sensor_line)
             self.last_send = timer()
 
     def process_sensor_line(self, line):
-        line =  line.strip().partition(":")[2].strip()
-        return f"{line} 0 0 0"
+        readings =  line.strip().partition(":")[2].strip().split(" ")
+        while len(readings) > 4:
+            readings += "0"
+        return " ".join(readings)
 
-sender = Sender()
 
-try:
-    while True:
-        for chint in ser.read():
-            sender.advance(chr(chint))
-except KeyboardInterrupt:
-    ser.close()
+def dummy():
+    x1 = random.randint(-10, 90)
+    x2 = random.randint(-10, 90)
+    x3 = random.randint(-10, 90)
+    x4 = random.randint(-10, 90)
+    send(f"{x1} {x2} {x3} {x4}")
+
+def main():
+    reader = SerialParser()
+    try:
+        while True:
+            for chint in ser.read():
+                reader.advance(chr(chint))
+    finally:
+        ser.close()
+if __name__ == "__main__":
+    main()
--- glove/glove.ino
+++ glove/glove.ino
@@ -1,12 +1,10 @@
 #include "init_strips.h"
-#include <WiFi.h>
-#include <HTTPClient.h>
 
 const float BOARD_VOLTAGE = 3.26;
 const bool DEBUG = true;
 const int FREQUENCY_HZ = 1;
 // const int DELAY_MS = 1000 / FREQUENCY_HZ;
-const int DELAY_MS = 1000;
+const int DELAY_MS = 100;
 
 const char* ssid = "511_A(starion)";
 const char* password = "zwykleHaslo";
@@ -17,8 +15,6 @@ float resistances[STRIP_NUM];
 float readResistance(Strip const& strip) {
   const float MAX_ANALOG_READ = 4095.0;
   int raw = analogRead(strip.pin);
-  Serial.print("raw ");
-  Serial.println(raw);
   float voltageAtSensor = (raw * BOARD_VOLTAGE) / MAX_ANALOG_READ;
   float factor = (BOARD_VOLTAGE / voltageAtSensor) - 1;
   return strip.knownResistor * factor;
@@ -26,9 +22,6 @@ float readResistance(Strip const& strip) {
 
 void setup() {
   Serial.begin(9600);
-  // Serial.println("Connecting...");
-  // WiFi.begin(ssid, password);
-  // while(Wisrintln(WiFi.localIP());
 
   for (int i = 0; i < STRIP_NUM; i++) {
     resistances[i] = 0.0;
@@ -40,34 +33,11 @@ void loop() {
     resistances[i] = readResistance(strips[i]);
   }
 
-  if (DEBUG) {
-    Serial.printf("R[%d]: %d", STRIP_NUM, (int)resistances[0]);
-    for (int i = 1; i < STRIP_NUM; i++) {
-      Serial.printf(", %d", (int)resistances[i]);
-    }
-    Serial.print("\n");
+  Serial.printf(">R[%d]:%d", STRIP_NUM, (int)resistances[0]);
+  for (int i = 1; i < STRIP_NUM; i++) {
+    Serial.printf(", %d", (int)resistances[i]);
   }
-
-  // if (WiFi.status() == WL_CONNECTED) {
-  //   WiFiClient client;
-  //   HTTPClient http;
-
-  //   http.begin(client, serverName);
-  //   http.addHeader("Content-Type", "text/plain");
-
-
-
-  //   String httpRequestData = "";
-  //   int httpResponseCode = http.POST("dupa");
-
-  //   Serial.print("HTTP Response code: ");
-  //   Serial.println(httpResponseCode);
-  //   http.end();
-  // }
+  Serial.print("\n");
 
   delay(DELAY_MS);
 }
-
-// curl -X POST -H "Content-Type: application/json" -H "Content-Length: 0" http://192.168.0.145:8080
-// curl -X POST -H "Content-Type: application/json" -H "Content-Length: 0" http://192.168.0.145:8080
-// curl -X POST -H "Content-Type: application/json" -d "number1=1&number2=2&number3=3&number4=4" http://192.168.0.145:8080
